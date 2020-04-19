3d70814d017231f40ff3c7d63542896bac30bfac
dennisclemente <dennisclemente@gmail.com>
2020-04-19 11:54:36 -0400

    first commit

---
+++ src/components/Home/Contact.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Title from "../Globals/Title";
+
+export default function Contact() {
+  return (
+    <section className="contact py-5">
+      <Title title="contact us" />
+      <div className="row">
+        <div className="col-10 col-sm-8 col-md-6 mx-auto">
+          <form
+            action="https://formspree.io/dennis@cinetropa.com"
+            method="POST"
+          >
+            {/* name */}
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                id="name"
+                placeholder="Jose Rizal"
+              />
+            </div>
+            {/* email */}
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                className="form-control"
+                name="email"
+                id="email"
+                placeholder="email@email"
+              />
+            </div>
+            {/* description */}
+            <div className="form-group">
+              <label htmlFor="description">Message</label>
+              <textarea
+                name="message"
+                id="message"
+                className="form-control"
+                rows="5"
+                placeholder="your message here..."
+              />
+            </div>
+            {/* submit */}
+            <button
+              type="submit"
+              className="btn btn-yellow btn-block text-capitalize mt-5"
+            >
+              submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}
